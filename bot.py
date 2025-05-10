1274d3ef6f9c3d5e5870a2cb10315053d1d020f4
Aibyn1234 <aibyn.nugmanov@yandex.ru>
2025-05-11 02:35:42 +0500

    add make_some

--- bot.py
+++ bot.py
@@ -34,4 +34,10 @@ def check_message_for_links(message):
             bot.ban_chat_member(chat_id, user_id)
             bot.reply_to(message, f"Пользователь @{message.from_user.username or 'без_ника'} был забанен за отправку ссылки.")
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+
 bot.infinity_polling(none_stop=True)
